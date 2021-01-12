1bc4dba1caefe9f43c688d9f1aa6899ddacc4a5b
berbaroovez <carterolson3@gmail.com>
2021-01-11 23:56:13 -0600

    nightly commit remade jack-white site using styled-compnents

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,99 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styled from "styled-components";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <HomePageContainer>
+      <div>
+        <JackText>JACK</JackText>
+        <WhiteText>WHITE</WhiteText>
+      </div>
+      <FrankIMG src="/images/frank.png" alt="a picture of frank kaminsky" />
+      <JackIMG src="/images/jack.png" alt="a picture of jack white squating" />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+      <HossIMG
+        src="/images/hoss.png"
+        alt="a picture of boys in Hoss Clothing"
+      />
+    </HomePageContainer>
+  );
 }
+
+const HomePageContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #e39d9c;
+  overflow: hidden;
+  text-align: center;
+  position: relative;
+
+  div {
+    padding-top: 15vh;
+    color: #f7ebe6;
+    line-height: 12vh;
+  }
+`;
+
+const JackText = styled.h1`
+  font-family: "Inter";
+  font-weight: 900;
+  margin: 0;
+  padding: 0;
+  font-size: 5rem;
+`;
+const WhiteText = styled.h1`
+  font-family: "Inter";
+  font-weight: 900;
+  margin: 0;
+  padding: 0;
+  font-size: 6.5rem;
+`;
+
+const JackIMG = styled.img`
+  position: absolute;
+  z-index: 2;
+  margin-right: auto;
+  margin-left: auto;
+  left: 5vw;
+  right: 0;
+  bottom: -20vh;
+  max-width: 95vw;
+  height: auto;
+  max-height: 700px;
+  @media only screen and (min-width: 740px) {
+    min-height: 700px;
+    max-width: 55%;
+    max-height: 900px;
+    left: 15vw;
+    bottom: -20vh;
+  }
+`;
+
+const FrankIMG = styled.img`
+  display: none;
+  position: absolute;
+  z-index: 1;
+  left: -200px;
+  bottom: 0;
+  max-width: 95vw;
+  height: auto;
+  max-height: 700px;
+  @media only screen and (min-width: 740px) {
+    display: unset;
+  }
+`;
+const HossIMG = styled.img`
+  display: none;
+  position: absolute;
+  z-index: 1;
+  right: -50px;
+  bottom: -3rem;
+  max-width: 55vw;
+  height: auto;
+
+  max-height: 700px;
+
+  @media only screen and (min-width: 740px) {
+    display: unset;
+  }
+`;
