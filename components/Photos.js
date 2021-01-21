4508a8d9bb38571e1aec2f2e8a4c677578153005
berbaroovez <carterolson3@gmail.com>
2021-01-21 11:40:12 -0600

    gallery

---
+++ components/Photos.js
@@ -0,0 +1,67 @@
+export const photos = [
+  {
+    src: "/gallery/boat.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "/gallery/boxing.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "/gallery/coast.jpg",
+    width: 1,
+    height: 1,
+  },
+  {
+    src: "/gallery/hossgolf.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "/gallery/jack.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    src: "/gallery/jackboat.jpg",
+    width: 5,
+    height: 3,
+  },
+  {
+    src: "/gallery/girl.jpg",
+    width: 3,
+    height: 2,
+  },
+  {
+    src: "/gallery/boxing.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "/gallery/coast.jpg",
+    width: 1,
+    height: 1,
+  },
+  {
+    src: "/gallery/hossgolf.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "/gallery/jack.jpg",
+    width: 3,
+    height: 4,
+  },
+  {
+    src: "/gallery/jackboat.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "/gallery/girl.jpg",
+    width: 2,
+    height: 2,
+  },
+];
--- pages/gallery.js
+++ pages/gallery.js
@@ -1,117 +1,153 @@
+import React, { useState, useCallback } from "react";
 import Layout from "../components/Layout";
 import styled from "styled-components";
-export default function Gallery() {
+import Gallery from "react-photo-gallery";
+import { photos } from "../components/Photos";
+import Carousel, { Modal, ModalGateway } from "react-images";
+
+export default function PhotoGallery() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
   return (
     <Layout>
-      <GalleryHeader>Gallery</GalleryHeader>
       <GalleryImageContainer>
-        <GalleryItem className="gallery-item-1">
-          <GalleryImage src="/gallery/boat.jpg" alt="placeholder" />
-        </GalleryItem>
-        <GalleryItem className="gallery-item-2">
-          <GalleryImage src="/gallery/boxing.jpg" alt="placeholder" />
-        </GalleryItem>
-        <GalleryItem className="gallery-item-3">
-          <GalleryImage src="/gallery/coast.jpg" alt="placeholder" />
-        </GalleryItem>
-        <GalleryItem className="gallery-item-4">
-          <GalleryImage src="/gallery/frank.jpg" alt="placeholder" />
-        </GalleryItem>
-        <GalleryItem className="gallery-item-5">
-          <GalleryImage src="/gallery/guy.jpg" alt="placeholder" />
-        </GalleryItem>
-        <GalleryItem className="gallery-item-6">
-          <GalleryImage src="/gallery/jack.jpg" alt="placeholder" />
-        </GalleryItem>
-        <GalleryItem className="gallery-item-7">
-          <GalleryImage src="/gallery/hossgolf.jpg" alt="placeholder" />
-        </GalleryItem>
-        <GalleryItem className="gallery-item-8">
-          <GalleryImage src="/gallery/engaged.jpg" alt="placeholder" />
-        </GalleryItem>
+        <Gallery photos={photos} onClick={openLightbox} />
+        <ModalGateway>
+          {viewerIsOpen ? (
+            <Modal onClose={closeLightbox}>
+              <Carousel
+                currentIndex={currentImage}
+                views={photos.map((x) => ({
+                  ...x,
+                  srcset: x.srcSet,
+                  caption: x.title,
+                }))}
+              />
+            </Modal>
+          ) : null}
+        </ModalGateway>
       </GalleryImageContainer>
     </Layout>
   );
 }
 
-const GalleryHeader = styled.h1`
-  margin-top: 4rem;
+// const GalleryHeader = styled.h1`
+//   margin-top: 4rem;
 
-  font-family: "Inter";
-  font-weight: 900;
-  font-size: 3rem;
-  color: white;
-  text-align: center;
-`;
+//   font-family: "Inter";
+//   font-weight: 900;
+//   font-size: 3rem;
+//   color: white;
+//   text-align: center;
+// `;
 
 const GalleryImageContainer = styled.div`
   padding: 0;
   max-width: 1000px;
-
+padding-top: 100px;
   margin: 0 auto;
+}
+//   display: grid;
+//   grid-template-columns: repeat(9, 1fr);
+//   grid-template-rows: repeat(20, 5vw);
+//   grid-gap: 15px;
 
-  display: grid;
-  grid-template-columns: repeat(9, 1fr);
-  grid-template-rows: repeat(20, 5vw);
-  grid-gap: 15px;
+//   .gallery-item-1 {
+//     grid-column-start: 1;
+//     grid-column-end: 6;
+//     grid-row-start: 1;
+//     grid-row-end: 5;
+//   }
 
-  .gallery-item-1 {
-    grid-column-start: 1;
-    grid-column-end: 6;
-    grid-row-start: 1;
-    grid-row-end: 5;
-  }
+//   .gallery-item-2 {
+//     grid-column-start: 6;
+//     grid-column-end: 10;
+//     grid-row-start: 1;
+//     grid-row-end: 7;
+//   }
+//   .gallery-item-3 {
+//     grid-column-start: 1;
+//     grid-column-end: 6;
+//     grid-row-start: 5;
+//     grid-row-end: 10;
+//   }
 
-  .gallery-item-2 {
-    grid-column-start: 6;
-    grid-column-end: 10;
-    grid-row-start: 1;
-    grid-row-end: 7;
-  }
-  .gallery-item-3 {
-    grid-column-start: 1;
-    grid-column-end: 6;
-    grid-row-start: 5;
-    grid-row-end: 10;
-  }
+//   .gallery-item-4 {
+//     grid-column-start: 6;
+//     grid-column-end: 10;
+//     grid-row-start: 7;
+//     grid-row-end: 11;
+//   }
+//   .gallery-item-5 {
+//     grid-column-start: 1;
+//     grid-column-end: 6;
+//     grid-row-start: 10;
+//     grid-row-end: 17;
+//   }
 
-  .gallery-item-4 {
-    grid-column-start: 6;
-    grid-column-end: 10;
-    grid-row-start: 7;
-    grid-row-end: 11;
-  }
-  .gallery-item-5 {
-    grid-column-start: 1;
-    grid-column-end: 6;
-    grid-row-start: 10;
-    grid-row-end: 17;
-  }
+//   .gallery-item-6 {
+//     grid-column-start: 6;
+//     grid-column-end: 10;
+//     grid-row-start: 11;
+//     grid-row-end: 14;
+//   }
 
-  .gallery-item-6 {
-    grid-column-start: 6;
-    grid-column-end: 10;
-    grid-row-start: 11;
-    grid-row-end: 14;
-  }
+//   .gallery-item-7 {
+//     grid-column-start: 6;
+//     grid-column-end: 10;
+//     grid-row-start: 14;
+//     grid-row-end: 17;
+//   }
+//   .gallery-item-8 {
+//     grid-column-start: 1;
+//     grid-column-end: 10;
+//     grid-row-start: 17;
+//     grid-row-end: 22;
+//   }
+// `;
+// const GalleryItem = styled.figure``;
 
-  .gallery-item-7 {
-    grid-column-start: 6;
-    grid-column-end: 10;
-    grid-row-start: 14;
-    grid-row-end: 17;
-  }
-  .gallery-item-8 {
-    grid-column-start: 1;
-    grid-column-end: 10;
-    grid-row-start: 17;
-    grid-row-end: 22;
-  }
-`;
-const GalleryItem = styled.figure``;
+// const GalleryImage = styled.img`
+//   width: 100%;
+//   height: 100%;
+//   object-fit: cover;
+// `;
 
-const GalleryImage = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`;
+// <GalleryHeader>Gallery</GalleryHeader>
+//       <GalleryImageContainer>
+//         <GalleryItem className="gallery-item-1">
+//           <GalleryImage src="/gallery/boat.jpg" alt="placeholder" />
+//         </GalleryItem>
+//         <GalleryItem className="gallery-item-2">
+//           <GalleryImage src="/gallery/boxing.jpg" alt="placeholder" />
+//         </GalleryItem>
+//         <GalleryItem className="gallery-item-3">
+//           <GalleryImage src="/gallery/coast.jpg" alt="placeholder" />
+//         </GalleryItem>
+//         <GalleryItem className="gallery-item-4">
+//           <GalleryImage src="/gallery/frank.jpg" alt="placeholder" />
+//         </GalleryItem>
+//         <GalleryItem className="gallery-item-5">
+//           <GalleryImage src="/gallery/guy.jpg" alt="placeholder" />
+//         </GalleryItem>
+//         <GalleryItem className="gallery-item-6">
+//           <GalleryImage src="/gallery/jack.jpg" alt="placeholder" />
+//         </GalleryItem>
+//         <GalleryItem className="gallery-item-7">
+//           <GalleryImage src="/gallery/hossgolf.jpg" alt="placeholder" />
+//         </GalleryItem>
+//         <GalleryItem className="gallery-item-8">
+//           <GalleryImage src="/gallery/engaged.jpg" alt="placeholder" />
+//         </GalleryItem>
+//       </GalleryImageContainer>
